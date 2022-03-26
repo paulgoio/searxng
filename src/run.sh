23735a57e2ebae23c18508ee1484ac8076e36118
mrpaulblack <paul@paulgo.io>
2022-03-26 10:07:16 +0100

    [fix] fix a leading colon in run.sh

--- src/run.sh
+++ src/run.sh
@@ -18,19 +18,19 @@ fi
 # proxy1 config based on PROXY1
 if [ ! -z "${PROXY1}" ]; then
     sed -i -e "s/  #  proxies:/  proxies:/g" \
-    -e "s+  #    all://:+    all://:\n      - ${PROXY1}:+g" \
+    -e "s+  #    all://:+    all://:\n      - ${PROXY1}+g" \
     searx/settings.yml;
 fi
 
 # proxy2 config based on PROXY2 (set this only when proxy1 is set)
 if [ ! -z "${PROXY2}" ]; then
-    sed -i -e "s+    all://:+    all://:\n      - ${PROXY2}:+g" \
+    sed -i -e "s+    all://:+    all://:\n      - ${PROXY2}+g" \
     searx/settings.yml;
 fi
 
 # proxy3 config based on PROXY3 (set this only when proxy1 is set)
 if [ ! -z "${PROXY3}" ]; then
-    sed -i -e "s+    all://:+    all://:\n      - ${PROXY3}:+g" \
+    sed -i -e "s+    all://:+    all://:\n      - ${PROXY3}+g" \
     searx/settings.yml;
 fi
 
