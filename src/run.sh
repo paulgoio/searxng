cdaca1ca098eb1e203671da5d16a1be90c27f936
mrpaulblack <paul@paulgo.io>
2025-07-20 13:33:58 +0200

    fix: granian entrypoint

--- src/run.sh
+++ src/run.sh
@@ -109,4 +109,4 @@ sed -i -e "s/ultrasecretkey/$(openssl rand -hex 16)/g" \
 searx/settings.yml
 
 # start uwsgi with SearXNG workload
-exec /usr/local/searxng/venv/bin/granian searx.webapp:app
+exec granian searx.webapp:app
