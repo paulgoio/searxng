998184083f506bf7910e1c3c64bfd8da78516779
mrpaulblack <paul@paulgo.io>
2026-08-29 08:30:10 +0000

    fix: include updated git submodule in renovate commit

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# this file is meant to be run by renovate when updating the git submodule upstream/
+set -e # <-- CRITICAL: Fail immediately if any command fails
 
+# this file is meant to be run by renovate when updating the git submodule upstream/
 echo "building theme from master branch searxng/searxng and update requirements.txt"
 
 echo "init and update submodule for upstream searxng"
@@ -21,7 +22,7 @@ echo "cp build files back to fork src folder"
 rm -rf src/css/*
 cp -r -v upstream/searx/static/themes/simple/*.css src/css/
 
-echo "update requirements from upstream searxng" 
+echo "update requirements from upstream searxng"
 cat upstream/requirements.txt upstream/requirements-server.txt > requirements.txt
 
 echo "cleanup upstream searxng submodule"
